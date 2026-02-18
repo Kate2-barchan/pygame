9db9a8bf9b684361dec82a85c5cc152a2092f10c
Kate2-barchan <katya.barchan@inbox.ru>
2026-02-18 19:22:57 +0400

    Update task13.py

--- task13.py
+++ task13.py
@@ -6,9 +6,10 @@ WIDTH, HEIGHT = 800, 600
 BLACK = (0, 0, 0)
 SPACE = (5, 5, 20)
 
-SHIP_SIZE = (100, 100)
-ASTEROID_SIZE = (109, 50)
-FIREBALL_SIZE = (25, 50)
+SHIP_SIZE = (120, 120)
+ASTEROID_SIZE = (169, 77)
+FIREBALL_SIZE = (50, 50)
+
 
 class Ship(pg.sprite.Sprite):
     speed = 6
@@ -43,13 +44,14 @@ class Asteroid(pg.sprite.Sprite):
         if self.rect.top > HEIGHT:
             self.kill()
 
+
 class Fireball(pg.sprite.Sprite):
     speed = 10
 
     def __init__(self, x, y):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.image.load(r"images\the fireball.png").convert_alpha()
-        self.image = pg.transform.rotate(self.image, FIREBALL_SIZE)
+        self.image = pg.transform.rotate(self.image, 90)
         self.image = pg.transform.scale(self.image, FIREBALL_SIZE)
         self.rect = self.image.get_rect(center=(x, y))
         self.mask = pg.mask.from_surface(self.image)
@@ -59,13 +61,11 @@ class Fireball(pg.sprite.Sprite):
         if self.rect.bottom < 0:
             self.kill()
 
+
 def spawn_asteroid():
     x = random.randint(20, WIDTH - 20)
     y = -40
-    asteroid = Asteroid(x, y)
-    all_sprites.add(asteroid)
-    asteroids.add(asteroid)
-
+    asteroids.add(Asteroid(x, y))
 
 
 pg.init()
@@ -79,15 +79,13 @@ pg.mixer.music.play(-1)
 crash_sound = pg.mixer.Sound(r"audio\collision_with_an_asteroid.mp3")
 fireball_sound = pg.mixer.Sound(r"audio\fireball_shot.flac")
 
-img = pg.image.load(r"images\background.jpg").convert
-img = pg.transform.scale(img, WIDTH, HEIGHT)
+bg = pg.image.load(r"images\background.jpg").convert()
+bg = pg.transform.scale(bg, (WIDTH, HEIGHT))
 
-sprites = pg.sprite.Group()
-asteroid = pg.sprite.Group()
-fireball = pg.sprite.Group
+asteroids = pg.sprite.Group()
+fireballs = pg.sprite.Group()
 
 ship = Ship()
-sprites.add(ship)
 
 spawn_timer = 0
 SPAWN_DELAY = 80
@@ -98,7 +96,6 @@ flag_play = True
 while flag_play:
     clock.tick(FPS)
 
-
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
@@ -106,12 +103,8 @@ while flag_play:
             break
         if event.type == pg.KEYDOWN and not game_over:
             if event.key == pg.K_SPACE:
-
-                fireball = Fireball(ship.rect.centerx, ship.rect.top)
-                all_sprites.add(bullet)
-                bullets.add(bullet)
-                shoot_sound.play()
-                
+                fireballs.add(Fireball(ship.rect.centerx, ship.rect.top))
+                fireball_sound.play()
     if not flag_play:
         break
 
@@ -129,29 +122,33 @@ while flag_play:
 
         ship.update(dx, dy)
 
-
         spawn_timer += 1
         if spawn_timer >= SPAWN_DELAY:
             spawn_timer = 0
             spawn_asteroid()
-            
-        fireball.update()
+
+        fireballs.update()
         asteroids.update()
-        
-        pg.sprite.groupcollide(fireball, asteroids, True, True, pg.sprite.collide_mask)
-        
+
+        pg.sprite.groupcollide(fireballs, asteroids, True, True, pg.sprite.collide_mask)
+
         if pg.sprite.spritecollideany(ship, asteroids, pg.sprite.collide_mask):
-            explode_sound.play()  # звук столкновения
+            crash_sound.play()  # звук столкновения
             pg.mixer.music.stop()
             game_over = True
 
-    screen.blit(img, (0, 0))
-    all_sprites.draw(screen)
-    
     if game_over:
         font = pg.font.SysFont(None, 72)
         text = font.render("GAME OVER", True, (255, 0, 0))
         rect = text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
+    # отрисовка
+    if not game_over:
+        screen.blit(bg, (0, 0))
+        ship.draw(screen)
+        fireballs.draw(screen)
+        asteroids.draw(screen)
+    else:
         screen.blit(text, rect)
 
     pg.display.update()
